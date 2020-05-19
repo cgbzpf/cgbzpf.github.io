e82c8a9fa880bd6a621f008df2d25185461795a9
cgbzpf <64100977+cgbzpf@users.noreply.github.com>
2020-05-19 14:48:52 +0200

    galleryapp
    new galleryapp mainpage #1

---
+++ galleryapp/script.js
@@ -0,0 +1,67 @@
+let girlData = {
+    photo: './images/buxa.jpg',
+    title: 'A vietnamese chick',
+    description: 'Na itt a kert',
+};
+
+$('#photo').attr('src', girlData.photo);
+$('#title').text(girlData.title);
+$('#description').text(girlData.description);
+
+let valleyData = {
+    photo: './images/rivervalley.jpg',
+    title: 'A valley',
+    description: 'Helo',
+};
+
+$('#photo').attr('src', valleyData.photo);
+$('#title').text(valleyData.title);
+$('#description').text(valleyData.description);
+
+let fieldData = {
+    photo: './images/ricefield.jpg',
+    title: 'A ricefield',
+    description: 'Jol latod',
+};
+
+$('#photo').attr('src', fieldData.photo);
+$('#title').text(fieldData.title);
+$('#description').text(fieldData.description);
+
+let bayData = {
+    photo: './images/halong.jpg',
+    title: 'Halong bay',
+    description: 'Yepp',
+};
+
+$('#photo').attr('src', bayData.photo);
+$('#title').text(bayData.title);
+$('#description').text(bayData.description);
+
+let currentPhoto = 0;
+let imagesData = [girlData, valleyData, fieldData, bayData];
+$('#photo').attr('src', imagesData[currentPhoto].photo)
+$('#title').text(imagesData[currentPhoto].title)
+$('#description').text(imagesData[currentPhoto].description)
+
+let loadPhoto = (photoNumber) => {
+    $('#photo').attr('src', imagesData[photoNumber].photo);
+    $('#title').text(imagesData[photoNumber].title);
+    $('#description').text(imagesData[photoNumber].description);
+  }
+  
+  loadPhoto(currentPhoto);
+
+  $('#leftarrow.arrowimg').click(() => {
+    currentPhoto--;
+    loadPhoto(currentPhoto);
+  })
+  
+  $('#rightarrow.arrowimg').click(() => {
+    currentPhoto++;
+    loadPhoto(currentPhoto);
+  })
+
+  imagesData.forEach((thumbnail, index) => {
+    $('#thumbnail-container').append(`<div class="thumbnail1" data-index="${index}">${currentPhoto} (data-index="${index}" src="${thumbnail.photo})</div>`);
+  });
